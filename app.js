12edc2c3465b1a46b4de0a6d596eb187f0ebd92f
Rayane Bengaoui <rayane.bengaoui@gmail.com>
2020-10-06 17:24:34 +0200

    Starting using scroll trigger for skills

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+const tl = gsap.timeline({
+  defaults: { ease: "power1.out" },
+});
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
 tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro-animation", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo(".splash", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+const tl_skill = gsap.timeline({
+  scrollTrigger: { trigger: ".skills-board" },
+  defaults: { ease: "power1.out" },
+});
+
+tl_skill.from(".skills-first", { x: 300, opacity: 0, duration: 1.5 });
